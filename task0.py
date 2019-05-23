82ec086d774a2d2fde135f59d7b2a675af99c995
nisheshmehta <nisheshmehta@dal.ca>
2019-05-23 06:09:37 -0700

    fizzbuzz

--- task0.py
+++ task0.py
@@ -2,3 +2,15 @@
 # this is secound commit
 #
 
+
+def fizzbuzzz(max_num=101):
+    for i in range(max_num):
+        value = ""
+        if i % 3 == 0: value += "Fizz"
+        if i % 5 == 0: value += "Buzz"
+        yield value if value else i
+
+for number, burp in enumerate(fizzbuzzz()):
+    print "%s: %s" % (number, burp)
+
+#https://github.com/nisheshmehta/fizzbuzz.git
